e8baecdb7072df5ec9fd9064b139c5b5115d66e5
Abdelfattah Saafan <toha70@gmail.com>
2020-05-07 02:46:40 -0400

    stop copy and paste

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ docker push toha70/multi-worker:$GIT_SHA
 kubectl apply -f ./k8s
 
 kubectl set image deployments/server-deployment server=toha70/multi-server:$SHA
-kubectl set image deployments/client-deployment server=toha70/multi-client:$GIT_SHA
-kubectl set image deployments/worker-deployment server=toha70/multi-worker:$GIT_SHA
+kubectl set image deployments/client-deployment client=toha70/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=toha70/multi-worker:$GIT_SHA
